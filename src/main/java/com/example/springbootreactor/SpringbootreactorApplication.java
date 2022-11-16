f2a1c84d7aff515ac127aa53138f675e8c45cdb0
Edu <emartinez.virtualcave@external.barcelo.com>
2022-11-16 22:01:38 +0100

    Terminada leccion 8: metodo suscribe

--- src/main/java/com/example/springbootreactor/SpringbootreactorApplication.java
+++ src/main/java/com/example/springbootreactor/SpringbootreactorApplication.java
@@ -1,5 +1,7 @@
 package com.example.springbootreactor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +10,24 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringbootreactorApplication implements CommandLineRunner {
 
+  private static final Logger log = LoggerFactory.getLogger(SpringbootreactorApplication.class);
+
   public static void main(String[] args) {
     SpringApplication.run(SpringbootreactorApplication.class, args);
   }
 
-	@Override
-	public void run(String... args) {
-		Flux<String> names = Flux.just("Andres", "Pedro", "Diego")
-				.doOnNext(System.out::println);
+  @Override
+  public void run(String... args) {
+    Flux<String> names = Flux.just("Andres", "Pedro", "Diego", "")
+        .doOnNext(name -> {
+              if (name.isEmpty()) {
+                throw new RuntimeException("Nombre no puede ser vacio");
+              } else {
+                System.out.println(name);
+              }
+            }
+        );
 
-		names.subscribe();
-
-	}
+    names.subscribe(log::info, error -> log.error(error.getMessage()));
+  }
 }
